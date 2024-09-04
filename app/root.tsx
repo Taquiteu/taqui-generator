e530016de322b3001c48fe8834aea41117913f80
Carlos Alberto Correia Jr <carlos.acj2009@hotmail.com>
2024-09-04 00:31:49 -0300

    feat: make the background spin!

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,7 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import "./tailwind.css";
+import { useEffect } from "react";
 
 export function links() {
   return [
@@ -18,18 +19,38 @@ export function links() {
 
 export function Layout({ children }: { children: React.ReactNode }) {
 
+  useEffect(() => {
+    function updateScaleFactor() {
+      const vw = window.innerWidth;
+      const vh = window.innerHeight;
+      const diagonal = Math.sqrt(vw * vw + vh * vh);
+      const scaleFactor = diagonal / Math.min(vw, vh);
+      document.body.style.setProperty('--scale-factor', `${scaleFactor}`);
+    }  
+    updateScaleFactor();  
+    window.addEventListener('resize', updateScaleFactor);
+    return () => {
+      window.removeEventListener('resize', updateScaleFactor);
+    };
+  }, []);
+
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=yes" />
         <Meta />
         <Links />
       </head>
-      <body className="bg-[url('/assets/taqui-o-background.png')] bg-center bg-no-repeat bg-cover">
-        {children}
-        <ScrollRestoration />
-        <Scripts />
+      <body className="relative overflow-hidden">
+        <div className="absolute inset-0 animate-spin-slower" style={{ height: '100vh' }}>
+        <div className="w-full h-full bg-[url('/assets/taqui-o-background.png')] bg-center bg-no-repeat bg-cover" style={{ transform: 'scale(var(--scale-factor))' }}></div>
+        </div>
+        <div className="relative">
+          {children}
+          <ScrollRestoration />
+          <Scripts />
+        </div>
       </body>
     </html>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,18 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        spin: 'spin 10s linear infinite',
+        'spin-slower': 'spin 120s linear infinite',
+      },
+      keyframes: {
+        spin: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
